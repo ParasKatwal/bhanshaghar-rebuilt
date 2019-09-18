1110268729dd8c31288d78d37cfbdb4ea6db1be6
raghshre <ra9havsh@gmail.com>
2019-09-18 09:03:03 +0545

    push

--- includes/array.php
+++ includes/array.php
@@ -1,5 +1,9 @@
 <?php
-
+    include_once('assets_back/array.class.php');
+    $menu_hot = new menu('hot drinks');
+    $menu_soft = new menu('soft drinks');
+    $menu_momo = new menu('momo');
+    
     $navitem = array(
         array(
             "link" => 'index.php',
@@ -19,84 +23,7 @@
         )
     );
 
-    $menuItem = array(
-        "espresso" => array(
-            "title" => "Espresso",
-            "price" => 4,
-            "img" => "espresso.jpg",
-            "blurb" => "Espresso is made by forcing very hot water under high pressure through finely ground compacted coffee. Tamping down the coffee promotes the water's even penetration through the grounds."
-        ),
-        "americano" => array(
-            "title" => "Americano",
-            "price" => 5,
-            "img" => "americano.jpg",
-            "blurb" => "Caffè Americano or Americano is a type of coffee drink prepared by diluting an espresso with hot water, giving it a similar strength to, but different flavor from, traditionally brewed coffee."
-        ),
-        "cappuccino" => array(
-            "title" => "Cappuccino",
-            "price" => 5,
-            "img" => "cappuccino.jpg",
-            "blurb" => "A cappuccino is an espresso-based coffee drink that originated in Italy, and is traditionally prepared with steamed milk foam."
-        ),
-        "latte" => array(
-            "title" => "Latte",
-            "price" => 4,
-            "img" => "latte.jpg",
-            "blurb" => "A latte is a coffee drink made with espresso and steamed milk. The term as used in English is a shortened form of the Italian caffè latte, caffelatte or caffellatte, which means milk coffee."
-        )
-    );
-    $menuItem2 = array(
-        "coke" => array(
-            "title" => "Coco-Cola",
-            "price" => 4,
-            "img" => "espresso.jpg",
-            "blurb" => "Espresso is made by forcing very hot water under high pressure through finely ground compacted coffee. Tamping down the coffee promotes the water's even penetration through the grounds."
-        ),
-        "sprite" => array(
-            "title" => "Sprite",
-            "price" => 5,
-            "img" => "americano.jpg",
-            "blurb" => "Caffè Americano or Americano is a type of coffee drink prepared by diluting an espresso with hot water, giving it a similar strength to, but different flavor from, traditionally brewed coffee."
-        ),
-        "fanta" => array(
-            "title" => "Fanta",
-            "price" => 5,
-            "img" => "cappuccino.jpg",
-            "blurb" => "A cappuccino is an espresso-based coffee drink that originated in Italy, and is traditionally prepared with steamed milk foam."
-        ),
-        "slice" => array(
-            "title" => "Slice",
-            "price" => 4,
-            "img" => "latte.jpg",
-            "blurb" => "A latte is a coffee drink made with espresso and steamed milk. The term as used in English is a shortened form of the Italian caffè latte, caffelatte or caffellatte, which means milk coffee."
-        )
-    );
-    $menuItem3 = array(
-        "Buff momo" => array(
-            "title" => "Buff Momo",
-            "price" => 4,
-            "img" => "espresso.jpg",
-            "blurb" => "Espresso is made by forcing very hot water under high pressure through finely ground compacted coffee. Tamping down the coffee promotes the water's even penetration through the grounds."
-        ),
-        "Veg momo" => array(
-            "title" => "Veg Momo",
-            "price" => 5,
-            "img" => "americano.jpg",
-            "blurb" => "Caffè Americano or Americano is a type of coffee drink prepared by diluting an espresso with hot water, giving it a similar strength to, but different flavor from, traditionally brewed coffee."
-        ),
-        "chiken momo" => array(
-            "title" => "Chiken Momo",
-            "price" => 5,
-            "img" => "cappuccino.jpg",
-            "blurb" => "A cappuccino is an espresso-based coffee drink that originated in Italy, and is traditionally prepared with steamed milk foam."
-        ),
-        "pork momo" => array(
-            "title" => "Pork Momo",
-            "price" => 4,
-            "img" => "latte.jpg",
-            "blurb" => "A latte is a coffee drink made with espresso and steamed milk. The term as used in English is a shortened form of the Italian caffè latte, caffelatte or caffellatte, which means milk coffee."
-        )
-    );
-    
-
+    $menuItem = $menu_hot->select();
+    $menuItem2 = $menu_soft->select();
+    $menuItem3 = $menu_momo->select(); 
 ?>
